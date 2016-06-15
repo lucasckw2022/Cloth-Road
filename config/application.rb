7301af0d8cbcee1b57ebdedd817ea76ca95846a4
yeeie201 <lucas20229763@hotmail.com>
2016-06-14 19:07:51 -0700

    testing

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module FinalProject
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile = false
 
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
